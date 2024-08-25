cae54d998030a115b263daad8f4337f52be8193d
Phong Vo <vhphong@gmail.com>
2024-08-25 11:04:12 -0500

    configuration kafka with docker

--- src/main/java/com/pnk/notification_service/controller/EmailController.java
+++ src/main/java/com/pnk/notification_service/controller/EmailController.java
@@ -7,6 +7,7 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +33,10 @@ public class EmailController {
                 .build();
     }
 
+
+    @KafkaListener(topics = "onboard-successful")
+    public void listen(String message) {
+        log.info(">> listen::message: {}", message);
+    }
+
 }
-
-
