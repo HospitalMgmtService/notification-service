33bb601b91eec36d02862f81739db893ad37e6db
Phong Vo <vhphong@gmail.com>
2024-08-25 22:58:14 -0500

    send email with kafka

--- src/main/java/com/pnk/notification_service/controller/EmailController.java
+++ src/main/java/com/pnk/notification_service/controller/EmailController.java
@@ -7,7 +7,6 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,12 +30,6 @@ public class EmailController {
         return ApiResponse.builder()
                 .result(emailService.deliverEmail(sendEmailRequest))
                 .build();
-    }
-
-
-    @KafkaListener(topics = "onboard-successful")
-    public void listen(String message) {
-        log.info(">> listen::message: {}", message);
     }
 
 }
